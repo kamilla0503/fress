62db8ff83a917a6b4eb5e2cad0b7496c061c8e83
kamilla0503 <kamilla.faizullina@yandex.ru>
2019-06-25 13:32:27 +0300

    +typedef

--- fress.h
+++ fress.h
@@ -15,10 +15,15 @@
 #include <map>
 #include <math.h>
 
+typedef std::vector <int> Sequence_t, Sequence;
+typedef std::pair<int, int> coord_t;
+typedef std::vector<coord_t> Conformation_t, Conformation;
+typedef std::map<int, std::vector<coord_t>>  contact_map_t ;
+typedef std::map<coord_t, int>  Map_coordinate_to_int ;
 
 class Protein{
 public:
-    std::vector <int> sequence;
+    Sequence sequence;
    // int number_of_iterations;
     int E; // энергия текущей конформации
     int min_E;
@@ -26,14 +31,12 @@ public:
     bool change_T; //чтобы корректно менять температуру каждый 50 000 шагов
     long int iterator; //для подсчета числа шагов
     std::vector <double>  probabilities={}; //вектор вероятностей для выбора длины, вероятность пропорциональна 1/l
-    std::vector <std::pair <int, int>> conformation;
+    Conformation conformation;
     std::vector<int> conformation_int;
-    std:: vector <std:: vector <std:: pair <int, int>>> results;
-    std:: map <int, std::vector < std::pair <int, int> >> map_of_contacts;
+    std:: vector <Conformation> results;
+    contact_map_t map_of_contacts;
     std:: map <int, std::pair<int, int>> map_int_to_coordinate;
-    std:: map <std::pair<int, int>, int> map_coordinate_to_int; //словарь для хранения всех соседей
-
-
+    Map_coordinate_to_int map_coordinate_to_int; //словарь для хранения всех соседей
 
     Protein();
     Protein(std::vector <int> sequence_input  );
@@ -41,18 +44,17 @@ public:
     void find_minimum();
     void calculate_probabilities_for_l(int lmin = 2, int lmax = 12);
     int count_contacts();
-    int count_contacts_partied_t( std::vector <int> &sequence, std::vector <std::pair <int, int>> &conformation, std:: map <int, std::vector < std::pair <int, int> >> &map_of_contacts, std:: map <std::pair<int, int>, int> &map_coordinate_to_int, int t, int current_energy    );
-
+    int count_contacts_dissected_t(Sequence_t &sequence, Conformation_t &conformation, contact_map_t &map_of_contacts,
+                                   Map_coordinate_to_int  &map_coordinate_to_int, int t, int current_energy);
     void regrowth_middle(int l, int start_position);
     void regrowth_start(int l );
     void regrowth_end(int l );
-    int distance( std:: pair <int, int> point1, std:: pair <int, int>point2   );
-
-
-
+    int distance( coord_t point1, coord_t point2   );
 };
 
-int count_contacts_breaked(std::vector <int> &sequence, std::vector <std::pair <int, int>> &conformation, std:: map <int, std::vector < std::pair <int, int> >> &map_of_contacts, std:: map <std::pair<int, int>, int> &map_coordinate_to_int     );
+int dissected(Sequence_t &sequence, Conformation_t &conformation,
+              contact_map_t &map_of_contacts,
+              Map_coordinate_to_int &map_coordinate_to_int);
 
 
 
